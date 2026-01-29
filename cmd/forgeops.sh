28fb0699553d4c9df679bcc0e221fe1bedbb649d
smachaka <smachaka@cisco.com>
2026-01-29 08:14:07 +0530

    feat: add ForgeOps CLI entry point

---
+++ cmd/forgeops.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+COMMAND=$1
+
+case "$COMMAND" in
+  version)
+    cat VERSION
+    ;;
+  *)
+    echo "ForgeOps CLI"
+    echo "Usage: forgeops [check|backup|deploy|cleanup|version]"
+    ;;
+esac
