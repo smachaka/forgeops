c26861a3de628c7d744413e8a5f85efb6d2aee21
smachaka <smachaka@cisco.com>
2026-01-29 14:15:08 +0530

    feat: wire system check into CLI

--- cmd/forgeops.sh
+++ cmd/forgeops.sh
@@ -7,6 +7,9 @@ case "$COMMAND" in
   version)
     cat VERSION
     ;;
+  check)
+    bash features/check/check.sh
+    ;;
   *)
     echo "ForgeOps CLI"
     echo "Usage: forgeops [check|backup|deploy|cleanup|version]"
